463fde284bb1516395109dc3a5f566a176f7745e
Vickson Rodrigues <55660155+vicksonrodrigues@users.noreply.github.com>
2022-06-04 01:57:26 +0530

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Player player;
+
+    public InputField textEntryField;
+    public Text logText;
+    public Text currentText;
+    public Image locationBackground;//UI backgroudImage
+
+    public Action[] actions;
+
+    [TextArea]
+    public string introText;
+
+	// Use this for initialization
+	void Start ()
+    {		
+        logText.text = introText;
+        DisplayLocation();
+        textEntryField.ActivateInputField();
+        DisplayBackground();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void DisplayBackground ()
+    {
+        
+        Sprite currentImage = player.currentLocation.backgroundImage ;
+        locationBackground.GetComponent<Image>().sprite = currentImage;
+        
+    }
+
+    public void DisplayLocation(bool additive = false)
+    {
+        string description = player.currentLocation.description+"\n";
+        description += player.currentLocation.GetConnectionsText();
+        description += player.currentLocation.GetItemsText();
+        if (additive)
+            currentText.text = currentText.text + "\n" + description;
+        else
+            currentText.text = description;
+    }
+
+
+
+    public void TextEntered()
+    {
+        LogCurrentText();
+        ProcessInput(textEntryField.text);
+        textEntryField.text = "";
+        textEntryField.ActivateInputField();
+
+    }
+
+    void LogCurrentText()
+    {
+        logText.text += "\n\n";
+        logText.text += currentText.text;
+
+        logText.text += "\n\n";
+        logText.text += "<color=#008000ff>" + textEntryField.text+"</color>";
+    }
+
+    void ProcessInput(string input)
+    {
+        input = input.ToLower();
+
+        char[] delimiter = {' '};
+        string[] separatedWords = input.Split(delimiter);
+
+        foreach(Action action in actions)
+        {
+            if (action.keyword.ToLower() == separatedWords[0])
+            {
+                if (separatedWords.Length>1)
+                {
+                    action.RespondToInput(this, separatedWords[1]);
+                }
+                else
+                {
+                    action.RespondToInput(this, "");
+                }
+                return;
+            }
+        }
+
+        currentText.text = "Nothing happens! (having trouble? type Help)";
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Return-of-Forgotten-King
+ 
